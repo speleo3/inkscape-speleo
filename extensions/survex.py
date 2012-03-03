da69169c2da8ab1eb02d33e591785d5f006c601d
Thomas Holder <speleo3@users.sourceforge.net>
2012-03-04 00:39:51 +0100

    add survex module

---
+++ extensions/survex.py
@@ -0,0 +1,430 @@
+'''
+Library to handle Survex 3D files (*.3d)
+
+Copyright (C) 2008-2011 Thomas Holder, http://sf.net/users/speleo3/
+Distributed under the terms of the GNU General Public License v2
+
+Example usage:
+
+    load 3d file
+    >>> from survex import Survex3D
+    >>> survey = Survex3D('all.3d')
+
+    number of stations
+    >>> print len(survey)
+    18066
+
+    number of stations with prefix 143.
+    >>> stations143 = list(survey.filter('143.'))
+    >>> print len(stations143)
+    383
+
+    number of legs
+    >>> print len(list(survey.iterlegs()))
+    18218
+
+    all labels of station p40a
+    >>> survey['p40a'].labels
+    ['laser.8_14', 'p40a', '40.eishohle.400', '40entlink2.14', '40to88.400']
+
+    length of shortest path from p40a to p41a in meters
+    >>> length, path = survey.shortestpath('p40a', 'p41a')
+    >>> print length / 100.0
+    1610.49206552
+
+    station properties
+    >>> survey['p40a'].is_surface()
+    True
+
+'''
+
+class Station(object):
+    '''
+    Survey station
+    '''
+    def __init__(self, xyz):
+        assert isinstance(xyz, tuple)
+        self.xyz = xyz
+        self.labels = []
+        self.connected_from = []
+        self.connected_to = []
+        self.flag = 0
+
+    def __repr__(self):
+        return '<%s %s%s>' % (self.__class__.__name__,
+                ', '.join(self.labels[:3]),
+                ', ...' if len(self.labels) > 3 else '')
+
+    def connect(self, other):
+        '''define a leg from other to self'''
+        self.connected_from.append(other)
+        other.connected_to.append(self)
+
+    def distance(self, other):
+        '''Euclidean distance to other station'''
+        return sum((a-b)**2 for (a,b) in zip(self.xyz, other.xyz))**0.5
+
+    @property
+    def connected(self):
+        '''Iterator over connected stations'''
+        from itertools import chain
+        return chain(self.connected_from, self.connected_to)
+
+    @property
+    def label(self):
+        '''(First) label of this station'''
+        if len(self.labels) == 0:
+            return '<unnamed station>'
+        return self.labels[0]
+
+    def is_surface(self):
+        return bool(self.flag & 0x01)
+    def is_underground(self):
+        return bool(self.flag & 0x02)
+    def is_entrance(self):
+        return bool(self.flag & 0x04)
+    def is_exported(self):
+        return bool(self.flag & 0x08)
+    def is_fixed(self):
+        return bool(self.flag & 0x10)
+
+    def shortestpath(self, other, verbose=0):
+        '''
+        Shortest Path between two stations. If verbose=2 then print path with
+        station names.
+        
+        Returns tuple of the length of the path (or -1 if no path is found) and
+        list of stations along the path.
+
+        Uses A* algorithm, adapted from Wikipedia:    
+        http://en.wikipedia.org/w/index.php?title=A*_search_algorithm&oldid=289896415
+        '''
+        def reconstruct_path(current_node):
+            if came_from.has_key(current_node):
+                p = reconstruct_path(came_from[current_node])
+                p.append(current_node)
+                return p
+            return [self]
+    
+        came_from = {}        # Map for backtrace (reconstruct_path)
+        closedset = set()     # The set of nodes already evaluated.
+        openset = [ self ]    # List sorted by f_score
+        g_score = { self: 0 } # Distance from self along optimal path.
+        h_score = { self: self.distance(other) } # Estimated lower bound from y to other
+        f_score = { self: h_score[self] }        # Estimated total distance from self to other through y.
+        while len(openset) > 0:
+            x = openset.pop(0)
+            if x == other:
+                path = reconstruct_path(other)
+                if verbose > 0:
+                    print 'Found path over %d stations' % (len(path))
+                if verbose > 1:
+                    print ' - '.join(s.label for s in path)
+                return g_score[other], path
+            closedset.add(x)
+            for y in x.connected:
+                if y in closedset:
+                    continue
+                tentative_g_score = g_score[x] + x.distance(y)
+                tentative_is_better = False
+                if y not in openset:
+                    h_score[y] = y.distance(other)
+                    tentative_is_better = True
+                elif tentative_g_score < g_score[y]:
+                    openset.remove(y)
+                    tentative_is_better = True
+                if tentative_is_better:
+                    came_from[y] = x
+                    g_score[y] = tentative_g_score
+                    f_score[y] = tentative_g_score + h_score[y]
+                    for i in xrange(len(openset)):
+                        if f_score[y] < f_score[openset[i]]:
+                            openset.insert(i, y)
+                            break
+                    else:
+                        openset.append(y)
+        if verbose:
+            print 'No path from %s to %s' % (self.label, other.label)
+        return -1, []
+
+class Survex3D(object):
+    '''
+    Datastructure to represent a "Survex 3D Image File".
+
+    http://trac.survex.com/browser/trunk/doc/3dformat.htm
+
+    Properties:
+     * Map of coords to stations (one station per unique xyz)
+     * Map of labels to stations (multiple labels per station possible)
+     * Iterator over stations
+
+    For each xyz position there is only one unique station, but a station
+    can have multiple labels.
+
+    It's in principle possible to load multiple 3d files into the same
+    instance, but only makes sense if there are no overlapping station
+    names.
+    '''
+    def __init__(self, filename=None):
+        self.clear()
+        if filename is None:
+            pass
+        elif isinstance(filename, basestring):
+            self.load(filename)
+        else:
+            # assume iterable with stations
+            for station in filename:
+                self.xyz2sta[station.xyz] = station
+            self._reindex()
+
+    def clear(self):
+        '''Remove all stations'''
+        self.title = '<unnamed survey>'
+        self.xyz2sta = {} # Map of xyz to stations
+        self.lab2sta = {} # Map of labels to stations
+        self.passages = [] # passages with LRUD data
+        self._prev = None
+        self._curr_label = ''
+
+    def _reindex(self):
+        self.lab2sta = {}
+        for station in self:
+            for label in station.labels:
+                self.lab2sta[label] = station
+
+    def __len__(self):
+        return len(self.xyz2sta)
+
+    def __repr__(self):
+        return '<%s "%s" %d stations>' % (self.__class__.__name__, self.title, len(self))
+
+    def __getitem__(self, key):
+        if isinstance(key, str):
+            return self.lab2sta[key]
+        if isinstance(key, tuple):
+            return self.xyz2sta[key]
+        raise TypeError('indices must be str or tuple')
+
+    def _move(self, xyz):
+        self._prev = self._get_or_new(xyz)
+
+    def _line(self, xyz):
+        assert self._prev != None
+        station = self._get_or_new(xyz)
+        station.connect(self._prev)
+        self._prev = station
+
+    def _label(self, xyz, flag=0):
+        station = self._get_or_new(xyz)
+        station.labels.append(self._curr_label)
+        self.lab2sta[self._curr_label] = station
+        if flag > 0:
+            station.flag = flag
+
+    def _lrud(self, lrud, flag=0):
+        self.passages.append((self._curr_label, lrud))
+        if flag & 0x01:
+            self.passages.append(None)
+ 
+    def extent(self):
+        '''
+        Get the boundig box of all stations as [[xmin, ymin, zmin], [xmax, ymax, zmax]]
+        '''
+        return [
+            [min(station.xyz[i] for station in self) for i in range(3)],
+            [max(station.xyz[i] for station in self) for i in range(3)],
+        ]
+
+    def filter(self, prefix):
+        '''
+        Iterator over stations that have a label starting with prefix.
+        '''
+        for station in self:
+            if any(label.startswith(prefix) for label in station.labels):
+                yield station
+
+    def _get_or_new(self, xyz):
+        '''Like self.xyz2sta.setdefault(xyz, Station(xyz))'''
+        station = self.xyz2sta.get(xyz)
+        if station is None:
+            station = self.xyz2sta[xyz] = Station(xyz)
+        return station
+
+    def iterstations(self):
+        '''Iterator over all stations'''
+        return self.xyz2sta.itervalues()
+ 
+    __iter__ = iterstations
+
+    def iterlegs(self, dosort=False):
+        '''Iterator over tuples of stations that are connected by a leg'''
+        for station in self:
+            for other in station.connected_from:
+                # station could be connected to another one that is not
+                # member of this instance, so better check
+                if other.xyz in self.xyz2sta:
+                    yield station, other
+
+    def iterlabels(self):
+        '''Iterator over all station labels'''
+        return iter(self.lab2sta)
+
+    def sortedlabels(self):
+        '''Naturally sorted list of labels'''
+        return sorted(self.lab2sta, key=natkey)
+
+    def print_points(self):
+        '''
+        Print stations as coords with station name to stdout, sorted by station
+        names.
+        '''
+        for label in self.sortedlabels():
+            station = self[label]
+            print '%6d %6d %6d ' % station.xyz, label
+
+    def print_lrud(self):
+        '''Print LRUD data'''
+        for xsect in self.passages:
+            if xsect is None:
+                print 'END'
+            else:
+                print xsect[0], xsect[1]
+
+    def shortestpath(self, key1, key2):
+        '''Shortest Path between two stations (see Station.shortestpath)'''
+        return self[key1].shortestpath(self[key2])
+
+    def load(self, filename):
+        '''
+        Load 3d file
+        '''
+        from struct import unpack
+
+        f = open(filename, 'rb')
+        line = f.readline() # File ID
+        if not line.startswith('Survex 3D Image File'):
+            raise IOError('not a Survex 3D File, aborting')
+
+        line = f.readline() # File format version
+        assert line[0] == 'v'
+        ff_version = int(line[1:])
+
+        self.title = unicode(f.readline().rstrip(), 'latin1') # Survex title
+        self.timestamp = f.readline().rstrip() # Timestamp
+
+        def read_xyz():
+            return unpack('<iii', f.read(12))
+
+        def read_len():
+            length = ord(f.read(1))
+            if length == 0xfe:
+                length += unpack('<H', f.read(2))[0]
+            elif length == 0xff:
+                length += unpack('<I', f.read(4))[0]
+            return length
+
+        def read_label():
+            length = read_len()
+            if length > 0:
+                self._curr_label += skip_bytes(length)
+
+        def skip_bytes(n):
+            return f.read(n)
+
+        while True:
+            byte = f.read(1)
+            if byte == '':
+                break
+
+            byte = ord(byte)
+
+            if byte == 0x00:
+                # STOP
+                self._curr_label = ''
+            elif byte <= 0x0e:
+                # TRIM
+                # FIXME: according to doc, trim 16 bytes, but img.c does 17!
+                (i,n) = (-17,0)
+                while n < byte:
+                    i -= 1
+                    if self._curr_label[i] == '.': n += 1
+                self._curr_label = self._curr_label[:i + 1]
+            elif byte <= 0x0f:
+                # MOVE
+                xyz = read_xyz()
+                self._move(xyz)
+            elif byte <= 0x1f:
+                # TRIM
+                self._curr_label = self._curr_label[:15 - byte]
+            elif byte <= 0x20:
+                # DATE
+                if ff_version < 7:
+                    skip_bytes(4)
+                else:
+                    skip_bytes(2)
+            elif byte <= 0x21:
+                # DATE
+                if ff_version < 7:
+                    skip_bytes(8)
+                else:
+                    skip_bytes(3)
+            elif byte <= 0x22:
+                # Error info
+                skip_bytes(5 * 4)
+            elif byte <= 0x23:
+                # DATE
+                skip_bytes(4)
+            elif byte <= 0x24:
+                # DATE
+                continue
+            elif byte <= 0x2f:
+                # Reserved
+                continue
+            elif byte <= 0x31:
+                # XSECT
+                read_label()
+                lrud = unpack('<hhhh', f.read(8))
+                self._lrud(lrud, byte & 0x01)
+            elif byte <= 0x33:
+                # XSECT
+                read_label()
+                lrud = unpack('<iiii', f.read(16))
+                self._lrud(lrud, byte & 0x01)
+            elif byte <= 0x3f:
+                # Reserved
+                continue
+            elif byte <= 0x7f:
+                # LABEL
+                read_label()
+                xyz = read_xyz()
+                self._label(xyz, byte & 0x3f)
+            elif byte <= 0xbf:
+                # LINE
+                read_label()
+                xyz = read_xyz()
+                self._line(xyz)
+            elif byte <= 0xff:
+                # Reserved
+                continue
+
+def natkey(s):
+    '''
+    Key function for "natural sorting" of strings.
+
+    Example:
+    >>> L = ['1', '10', '2', '1a']
+    >>> sorted(L, key=natkey)
+    ['1', '1a', '2', '10']
+    '''
+    L = len(s)
+    if L == 0:
+        return tuple()
+    r, i, d = [], 0, s[0].isdigit()
+    for j in range(L):
+        if d != s[j].isdigit():
+            r.append(int(s[i:j]) if d else s[i:j].lower())
+            i, d = j, not d
+    r.append(int(s[i:]) if d else s[i:].lower())
+    return r
+
+# vi:expandtab:smarttab
