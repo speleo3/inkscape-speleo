93c8258330a99b2f810b5e58e46ac78be55ab07d
Thomas Holder <thomas@thomas-holder.de>
2025-02-26 16:06:35 +0100

    Test for th2setprops.py

---
+++ tests/test_th2setprops.py
@@ -0,0 +1,47 @@
+import th2setprops as m
+import os
+import pytest
+import subprocess
+import sys
+from pathlib import Path
+from lxml import etree
+
+TESTS_DATA = Path(__file__).resolve().parent / "data"
+
+ns = {
+    "svg": "http://www.w3.org/2000/svg",
+    "xlink": "http://www.w3.org/1999/xlink",
+}
+
+
+@pytest.mark.parametrize("ioencoding", [
+    "utf-8",
+    "cp1252",
+])
+def test_th2setprops(ioencoding, tmp_path):
+    env = os.environ.copy()
+    env["PYTHONIOENCODING"] = ioencoding
+    env["PYTHONUTF8"] = "1" if ioencoding == "utf-8" else "0"
+    env["LC_ALL"] = f"C.{ioencoding}"
+    path_input = TESTS_DATA / "ink2.svg"
+    path_output = tmp_path / "output.svg"
+    args = [
+        sys.executable,
+        m.__file__,
+        "--role=point",
+        "--type=height",
+        "--dropstyle=true",
+        "--id=rect1",
+        "--output=" + str(path_output),
+        str(path_input),
+    ]
+    subprocess.check_call(args, env=env)
+    tree = etree.fromstring(path_output.read_bytes())
+
+    texts = tree.xpath("//svg:symbol[@id='point-height']/svg:text//text()",
+                       namespaces=ns)
+    assert "".join(texts) == "±X"
+
+    nodes = tree.xpath("//svg:use[@xlink:href='#point-height']", namespaces=ns)
+    assert len(nodes) == 1
+    assert nodes[0].attrib["x"].rstrip(".0") == "35"
